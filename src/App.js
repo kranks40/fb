7d8deb988a1360ac31b0cd7d72b19138ef778588
kranks40 <eng_peters20@yahoo.com>
2020-08-29 11:03:38 -0400

    Completed the Header and now moving to the Sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     //BEM naming convention
-    <div className="app">
-      <h1>This is Facebook Clone</h1>
+    <div className="app">      
+      <Header/>
+
+      <div className='app__body'>
+        <Sidebar/>
+      </div>
     </div>
   );
 }
