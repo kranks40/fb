6b0dc555f0fe9c1a1514985f46597beb52a0e16d
kranks40 <eng_peters20@yahoo.com>
2020-08-29 06:44:24 -0400

    Created header and firebase component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAmyxT3G7H1b28nbw_EEf85UISppM_WVq0",
+    authDomain: "fb-clone-fabc5.firebaseapp.com",
+    databaseURL: "https://fb-clone-fabc5.firebaseio.com",
+    projectId: "fb-clone-fabc5",
+    storageBucket: "fb-clone-fabc5.appspot.com",
+    messagingSenderId: "193977449128",
+    appId: "1:193977449128:web:eb35818a15340eb5dd609d",
+    measurementId: "G-TGC87VPP3X",
+  };
